34cef212b4cbcb338d3a3816e32411bd0e6b4564
Siddhant <gavalisiddhant@gmail.com>
2024-07-14 20:32:53 +0530

    day2

--- script.js
+++ script.js
@@ -16,35 +16,161 @@
 // #### Activity 5: Understanding const
 // 6. Try reassigning a variable declared with const and observe the error.
 
-var a=5
-console.log(a)
+// var a=5
+// console.log(a)
 
-let str="Hello"
-console.log(str)
+// let str="Hello"
+// console.log(str)
 
-const b=true;
-console.log(b)
+// const b=true;
+// console.log(b)
 
-let c=4
-let d="hello"
-let e=true
-let f={1:2}
-let g=["siddhant"]
-console.log(typeof(c))
-console.log(typeof(d))
-console.log(typeof(e))
-console.log(typeof(f))
-console.log(typeof(g))
-
-
-let n1=5
-console.log(n1)
- n1=6
-console.log(n1)
-
-const n2=75
-console.log(n2)
-n2=74
-console.log(n2)
+// let c=4
+// let d="hello"
+// let e=true
+// let f={1:2}
+// let g=["siddhant"]
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+// console.log(typeof(f))
+// console.log(typeof(g))
 
 
+// let n1=5
+// console.log(n1)
+//  n1=6
+// console.log(n1)
+
+// const n2=75
+// console.log(n2)
+// n2=74
+// console.log(n2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Activity 1: Arithmetic Operations
+
+// Task 1: Write a program to add two numbers and log the result to the console.
+let a=5
+let b=6
+let c=a+b
+console.log(c)
+ 
+
+
+// Task 2: Write a program to subtract two numbers and log the result to the console. Task 3: Write a program to multiply two numbers and log the result to the console.
+let d=5
+let e=6
+let f=d*e
+console.log(f)
+// Task 4: Write a program to divide two numbers and log the result to the console.
+    let g=5
+    let h=6
+    let i=g/h
+    console.log(i)
+
+// • Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let j=5
+let k=6
+let l=j%k
+console.log(l)
+
+// Activity 2: Assignment Operators
+
+// Task 6: Use the + operator to add a number to a variable and log the result to the console.
+
+let m=5
+let n=6
+let o=m+n
+console.log(o)
+
+// • Task 7: Use the operator to subtract a number from a variable and log the result to the console.
+
+let p=5
+let q=6
+let r=p-q
+console.log(r)
+// Activity 3: Comparison Operators
+
+// Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let s=5
+let t=6
+let u=s<t
+console.log(u)
+
+
+// Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+
+let v=5     
+let w=6
+let x=v<=w
+console.log(x)
+
+// Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+
+let y=5
+let z=6
+let a1=y==z
+console.log(a1)
+
+let b1=5
+let c1=6
+let d1=b1===c1
+console.log(d1)
+
+
+// Activity 4: Logical Operators
+
+// Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console. 
+let e1=5
+let f1=6
+if (e1==5 && f1==6){
+    console.log(true)
+}
+
+
+// Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+
+let g1=5
+let h1=34
+if (g1==5 || h1==6){
+    console.log(true)
+}
+
+// • Task 13: Write a program that uses the operator to negate a condition and log the result to the console.
+  a3=true
+  ~a3
+  console.log(a3)
+
+
+// Activity 5: Ternary Operator
+
+// • Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+
+let i1 = 5;
+(i1 % 2 ==0) ? console.log("Even") : console.log("Odd")
